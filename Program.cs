0ebf7a172d5c022d8a8d555392fa01f55979c770
qianwang <wangqianlf@live.com>
2024-02-22 15:31:32 +0100

    ajout des directives

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Voici, mon prmier programme en C#");
-Console.ReadKey();
+using System;
+
+namespace console_app // Remplacez MonNamespace par le nom de votre namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voici, mon prmier programme en C#");
+            Console.ReadKey();
+        }
+    }
+}

930721716c351f20dcd8b84cd5e52fe349117d5b
qianwang <wangqianlf@live.com>
2024-02-22 23:14:30 +0100

    ajout .fake dans gitignore et variables

--- Program.cs
+++ Program.cs
@@ -8,6 +8,20 @@ namespace console_app // Remplacez MonNamespace par le nom de votre namespace
         static void Main(string[] args)
         {
             Console.WriteLine("Voici, mon prmier programme en C#");
+            // Boolean
+            bool homme = true;
+            Console.WriteLine(homme);
+            // Integer
+            int age = 29;
+            Console.WriteLine(age);
+            //Float
+            float taille = 1.80f;
+            Console.WriteLine(taille);
+            //String
+            string prenom = "Qian";
+            Console.WriteLine(prenom);
+            prenom = "Etienne";
+            Console.WriteLine(prenom);
             Console.ReadKey();
         }
     }
